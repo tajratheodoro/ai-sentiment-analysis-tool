0977c6e7e354c3eec28c3851fe32da6eedafcdad
Theodoro Tajra <tajratheodoro0@outlook.com>
2026-04-21 20:02:44 -0300

    refactor: internacionaliza assinaturas de métodos e consolidação de relatórios
    Renomeia o método de geração de relatórios e padroniza as variáveis internas e a string de saída para o idioma inglês. Isso garante consistência semântica total com o motor de NLP da TextBlob e com o restante da aplicação.

--- main.py
+++ main.py
@@ -5,21 +5,40 @@ from time import sleep
 ia = SentimentAnalyzer()
 
 print("Welcome to the customer feedback analysis system!\n")
-cliente1 = Feedback("The whatsapp bot is amazing and very fast!", 101)
-cliente2 = Feedback("I had a terrible experience, it keeps crashing.", 102)
-cliente3 = Feedback("It is a decent tool, but could be better.", 103)
-cliente4 = Feedback("The customer service is good, but it could be better.", 104)
 
-print("Beginning the analysis of customer feedback...\n")
-sleep(2)
+limit = 3
+counter = 0 
 
-ia.analisar(cliente1)
-ia.analisar(cliente2)
-ia.analisar(cliente3)
-ia.analisar(cliente4)
+while counter < limit:
+    user_input = input("Please, enter your feedback (or type 'exit' to finish): ")
+    if user_input.lower() == 'exit':
+        break
 
-print(f"Resultado da análise do cliente 1: {cliente1.nota}  ")
-print(f"Resultado da análise do cliente 2: {cliente2.nota}  ")
-print(f"Resultado da análise do cliente 3: {cliente3.nota}  ")
-print(f"Resultado da análise do cliente 4: {cliente4.nota}  ")
-print(ia.gerar_relatorio())
+    valid_user_id = False
+    while not valid_user_id:
+        id_input = input("Please, enter your user ID (numeric): ")
+
+        if id_input.isdigit():
+            id_num = int(id_input)
+
+            existing_ids = [f.user_id for f in ia.historical_analysis]
+
+            if id_num in existing_ids:
+                print("Error: This user ID has already in use! Please choose a different one.")
+            else:
+                valid_user_id = True
+
+        else:
+            print("Invalid user ID. Please enter a numeric value.")
+            continue
+
+        print("Beginning the analysis of customer feedback...\n")
+        sleep(2)
+
+        new_client = Feedback(user_input, id_num)
+        ia.analisar(new_client)
+        print("Analysis completed for the current feedback.\n")
+
+        counter += 1
+
+print(ia.generate_report())
