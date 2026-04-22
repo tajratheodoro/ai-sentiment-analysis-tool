8ca58aa6600e7fab58c890968e3b3c0760f95a6b
Theodoro Tajra <tajratheodoro0@outlook.com>
2026-04-22 08:31:41 -0300

    feat: integra banco de dados SQLite para persistência do histórico da IA.
    - Implementa operações nativas de banco (Criação de Tabela, Leitura, Inserção e Deleção).
    - Utiliza consultas parametrizadas (bind variables) no INSERT, blindando a aplicação contra ataques de SQL Injection.
    - Desenvolve funcionalidade de 'Reset' de banco gerenciada pelo usuário via UI, acionando recarregamento de estado (st.rerun) para garantir uma UX fluida.

---
+++ database.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self, db_name="feedback_analysis.db"):
+        self.db_name = db_name
+        self.setup_database()
+
+    def setup_database(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS historical_analysis (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        feedback TEXT NOT NULL,
+                        sentiment TEXT NOT NULL
+                       )
+                    """)
+        conn.commit()   
+        conn.close()
+
+    def save_feedback(self, feedback, sentiment):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO historical_analysis (feedback, sentiment) VALUES (?, ?)
+                       """, (feedback, sentiment))
+        conn.commit()
+        conn.close()
+    
+    def get_all_analysis(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM historical_analysis")
+        # O comando fetchall() pega as linhas que o execute encontrou e guarda numa lista
+        rows = cursor.fetchall()
+        conn.close()
+        return rows
+    
+    def clear_database(self):
+        conn = sqlite3.connect(self.db_name)
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM historical_analysis")
+        conn.commit()
+        conn.close()
